3671e0556479ab38d4b5bb1ef0b7321fe7f54506
MartinInformatikk <martinmalo2003@gmail.com>
2024-01-18 08:18:20 +0100

    Started on javascript
    Added javascript for displaying articles dynamically, and started on shopping cart.

--- shopscript.js
+++ shopscript.js
@@ -1,3 +1,14 @@
+console.log(products);
 
+let productsHTML = "<h2>Ninjago</h2>";
 
+products.map(product => productsHTML += `<article>
+<img src="website_images/PROD_${product.imagefile}" alt="${product.title}"/>
+<a href="#">${product.category}</a>
+<h3>${product.title}</h3>
+<span>Kr. ${product.price}</span>
+<button>Legg i handlekurv</button>
+</article>`);
 
+const main = document.getElementsByTagName("main");
+main[0].innerHTML = productsHTML;
